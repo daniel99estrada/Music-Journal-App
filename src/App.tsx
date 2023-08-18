f5bacb3cbf0b47b6f9c8e4c8c66f23b1011a037f
Daniel Estrada <pildoestrada@gmail.com>
2023-08-17 20:45:28 -0400

    added spotify auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import JournalEntryForm from './JournalEntryForm';
 import Playlist from './Playlist';
 
 const POSTJOURNALURL = 'https://y3trlbyznl.execute-api.us-east-1.amazonaws.com/dev/postjournal';
+
+const CLIENT_ID = "745548ae2ecf4f858ddcac85a3ba5f41";
+const SPOTIFY_AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+const REDIRECT_URL_AFTER_LOGIN = "http://localhost:5175/";
+const SCOPES = ["user-read-currently-playing", "user-top-read", "user-library-modify"];
+
+const SCOPES_URL_PARAM = SCOPES.join("%20");
 
 const App: React.FC = () => {
   const [sentiments, setSentiments] = useState<{
@@ -16,6 +23,28 @@ const App: React.FC = () => {
     Neutral: 0,
     Mixed: 0,
   });
+
+  const [params, setParams] = useState<{ [key: string]: string } | null>(null);
+
+  const getReturnedParamsFromSpotifyAuth = () => {
+    const hash = window.location.hash;
+    const hashParams = hash.substring(1).split("&");
+    const parameters: { [key: string]: string } = {}; // Specify index signature
+
+    for (let param of hashParams) {
+      const [key, value] = param.split("=");
+      parameters[key] = value;
+    }
+
+    return parameters;
+  };
+
+  useEffect(() => {
+    const urlParams = getReturnedParamsFromSpotifyAuth();
+    setParams(urlParams);
+    console.log(params);
+  }, []);
+
   const handleJournalSubmit = async (entry: string) => {
     console.log('Journal entry submitted:', entry);
 
@@ -43,11 +72,24 @@ const App: React.FC = () => {
     }
   };
 
+  const handleSpotifyLogin = () => {
+    const authUrl = `${SPOTIFY_AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPES_URL_PARAM}&response_type=token`;
+    window.location.href = authUrl;
+  };
+
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-4xl font-bold mb-4">Journal App</h1>
+      <button onClick={handleSpotifyLogin} className="bg-blue-500 text-white p-2 rounded">Login with Spotify</button>
       <JournalEntryForm onSubmit={handleJournalSubmit} />
       <Playlist sentiments={sentiments} />
+      {params && (
+        <div>
+          <h2>Params:</h2>
+          <pre>{JSON.stringify(params, null, 2)}</pre>
+        </div>
+      )}
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.tsx'
-import './index.css'
+import App from './App'; // Make sure the path is correct
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// This line will render your app inside the element with the ID 'root'
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
