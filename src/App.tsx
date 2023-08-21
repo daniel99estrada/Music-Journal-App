dce4bc6360f6f2e11076ebeb5559af1bcd5c951d
Daniel Estrada <pildoestrada@gmail.com>
2023-08-21 19:02:10 -0400

    method to call the spotify api

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ const POSTJOURNALURL = 'https://y3trlbyznl.execute-api.us-east-1.amazonaws.com/d
 
 const CLIENT_ID = "745548ae2ecf4f858ddcac85a3ba5f41";
 const SPOTIFY_AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-const REDIRECT_URL_AFTER_LOGIN = "http://localhost:5175/";
+const REDIRECT_URL_AFTER_LOGIN = "http://localhost:5173/";
 const SCOPES = ["user-read-currently-playing", "user-top-read", "user-library-modify"];
 
+// "user-library-modify"
 const SCOPES_URL_PARAM = SCOPES.join("%20");
+
 
 const App: React.FC = () => {
   const [sentiments, setSentiments] = useState<{
@@ -24,26 +26,56 @@ const App: React.FC = () => {
     Mixed: 0,
   });
 
-  const [params, setParams] = useState<{ [key: string]: string } | null>(null);
+  // const getReturnedParamsFromSpotifyAuth = (): SpotifyParameters => {
+  //   const hash = window.location.hash;
+  //   const hashParams = hash.substring(1).split("&");
+  //   const parameters: SpotifyParameters = {};
+  
+  //   for (let param of hashParams) {
+  //     const [key, value] = param.split("=");
+  //     parameters[key] = value;
+  //   }
+  
+  //   return parameters;
+  // };
 
-  const getReturnedParamsFromSpotifyAuth = () => {
-    const hash = window.location.hash;
-    const hashParams = hash.substring(1).split("&");
-    const parameters: { [key: string]: string } = {}; // Specify index signature
-
-    for (let param of hashParams) {
-      const [key, value] = param.split("=");
-      parameters[key] = value;
-    }
-
-    return parameters;
-  };
+  const [spotifyParams, setSpotifyParams] = useState<{ [key: string]: string }>({});
 
   useEffect(() => {
-    const urlParams = getReturnedParamsFromSpotifyAuth();
-    setParams(urlParams);
-    console.log(params);
+    // Parse the URL to extract all parameters
+    const params = new URLSearchParams(window.location.hash.substring(1));
+    const paramsObject: { [key: string]: string } = {};
+    params.forEach((value, key) => {
+      paramsObject[key] = value;
+    });
+    setSpotifyParams(paramsObject);
+
+    // If access token is present, fetch top artists
+    if (paramsObject.access_token) {
+      fetchTopArtists();
+    }
   }, []);
+
+  const fetchTopArtists = () => {
+    fetch('https://api.spotify.com/v1/me/top/artists', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${spotifyParams.access_token}` // Use access_token key
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Top Artists API Response:', data); // Log the API response
+    })
+    .catch(error => {
+      console.error('Error fetching top artists:', error);
+    });
+  };
+
+  // useEffect(() => {
+  //   const params = getReturnedParamsFromSpotifyAuth();
+  //   setSpotifyParams(params);
+  // }, []);
 
   const handleJournalSubmit = async (entry: string) => {
     console.log('Journal entry submitted:', entry);
@@ -70,6 +102,10 @@ const App: React.FC = () => {
     } catch (error) {
       console.error('Error sending journal entry:', error);
     }
+
+    if (spotifyParams.access_token) {
+      fetchTopArtists();
+    }
   };
 
   const handleSpotifyLogin = () => {
@@ -81,17 +117,17 @@ const App: React.FC = () => {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-4xl font-bold mb-4">Journal App</h1>
-      <button onClick={handleSpotifyLogin} className="bg-blue-500 text-white p-2 rounded">Login with Spotify</button>
-      <JournalEntryForm onSubmit={handleJournalSubmit} />
-      <Playlist sentiments={sentiments} />
-      {params && (
+      {spotifyParams.access_token ? (
         <div>
-          <h2>Params:</h2>
-          <pre>{JSON.stringify(params, null, 2)}</pre>
+          
+          <JournalEntryForm onSubmit={handleJournalSubmit} />
+          <Playlist sentiments={sentiments} />
+          <button onClick={fetchTopArtists} className="bg-blue-500 text-white p-2 rounded mt-8">Get Spotify Data</button>
         </div>
+      ) : (
+        <button onClick={handleSpotifyLogin} className="bg-blue-500 text-white p-2 rounded">Login with Spotify</button>
       )}
     </div>
   );
-};
-
+}
 export default App;
