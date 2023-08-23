27fcccd461f55c2f199d9ab36383738b33be0edc
Daniel Estrada <pildoestrada@gmail.com>
2023-08-23 17:15:37 -0400

    visualized the playlist response

--- src/App.tsx
+++ src/App.tsx
@@ -8,42 +8,21 @@ const POSTSPOTIFYURL = 'https://y3trlbyznl.execute-api.us-east-1.amazonaws.com/d
 const CLIENT_ID = "745548ae2ecf4f858ddcac85a3ba5f41";
 const SPOTIFY_AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const REDIRECT_URL_AFTER_LOGIN = "http://localhost:5173/";
-// const SCOPES = ["user-read-currently-playing", "user-top-read", "user-library-modify"];
-const SCOPES = ["user-read-currently-playing", "user-top-read"];
+const SCOPES = ["user-read-currently-playing", "user-top-read", "user-library-modify"];
+// const SCOPES = ["user-read-currently-playing", "user-top-read"];
 
 // "user-library-modify"
 const SCOPES_URL_PARAM = SCOPES.join("%20");
 
-interface responseAPI {
+interface TrackInfo {
   name: string;
+  artist: string;
+  spotifyURL: string;
+  imageURL: string;
 }
-
-interface Recommendation {
-  tracks: Track[];
-}
-
-const response: Recommendation = {
-  tracks: [
-    { name: 'hello' },
-    { name: 'yo' }
-  ]
-};
 
 const App: React.FC = () => {
-  const [recommendedTrackNames, setRecommendedTrackNames] = useState<string[]>(["yo", "yo"]);
-  
-  const [sentiments, setSentiments] = useState<{
-    Positive: number;
-    Negative: number;
-    Neutral: number;
-    Mixed: number;
-  }>({
-    Positive: 0,
-    Negative: 0,
-    Neutral: 0,
-    Mixed: 0,
-  });
-
+  const [tracks, setTracks] = useState<TrackInfo[]>();
   const [spotifyParams, setSpotifyParams] = useState<{ [key: string]: string }>({});
 
   useEffect(() => {
@@ -69,7 +48,24 @@ const App: React.FC = () => {
             body: requestBody,
         });
         const responseData = await response.json();
+
         console.log('SPotify API response:', responseData);
+
+        const trackData = responseData.recommendations.tracks;
+        const newTracks: TrackInfo[] = [];
+
+        for (const track of trackData) {
+          const newTrack: TrackInfo = {
+            name: track.album.name,
+            artist: track.album.artists[0].name,
+            spotifyURL: track.album.external_urls.spotify,
+            imageURL: track.album.images[0].url,
+          };
+          newTracks.push(newTrack);
+        }
+
+        setTracks(newTracks);
+
     } catch (error) {
         console.error('Error connecting to Spotify', error);
     }
@@ -95,7 +91,6 @@ const App: React.FC = () => {
       // Parse and log the response data
       const responseData = await response.json();
       console.log('API response:', responseData);
-      setSentiments(responseData.sentiments);
 
       // You can perform any additional logic or state updates here based on the response
     } catch (error) {
@@ -114,17 +109,22 @@ const App: React.FC = () => {
 
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-4xl font-bold mb-4">Journal App</h1>
-      {spotifyParams.access_token ? (
-        <div>
-          <JournalEntryForm onSubmit={handleJournalSubmit} />
-          <Playlist recommendedTrackNames={recommendedTrackNames} />
-        </div>
-      ) : (
-        <button onClick={handleSpotifyLogin} className="bg-blue-500 text-white p-2 rounded">Login with Spotify</button>
-      )}
+    <div className="bg-slate-100 min-h-screen">
+      <div className="container mx-auto p-4">
+        <h1 className="text-4xl font-bold mb-4">Journal App</h1>
+        {spotifyParams.access_token ? (
+          <div>
+            <JournalEntryForm onSubmit={handleJournalSubmit} />
+            <Playlist playlistTracks={tracks} />
+          </div>
+        ) : (
+          <button onClick={handleSpotifyLogin} className="bg-blue-500 text-white p-2 rounded">
+            Login with Spotify
+          </button>
+        )}
+      </div>
     </div>
   );
+  
 }
 export default App;
--- src/Playlist.tsx
+++ src/Playlist.tsx
@@ -1,20 +1,39 @@
 import React from 'react';
 
-interface PlaylistProps {
-  recommendedTrackNames: string[];
+interface TrackInfo {
+  name: string;
+  artist: string;
+  spotifyURL: string;
+  imageURL: string;
 }
 
-const Playlist: React.FC<PlaylistProps> = ({ recommendedTrackNames }) => {
+interface PlaylistProps {
+  playlistTracks?: TrackInfo[]; // Make the prop optional
+}
+
+const Playlist: React.FC<PlaylistProps> = ({ playlistTracks }) => {
   return (
-    <div className="mt-4">
-      <h2 className="text-xl font-semibold mb-2">Recommended Tracks:</h2>
-      <ul className="list-disc pl-6">
-        {recommendedTrackNames.map((trackName, index) => (
-          <li key={index} className="mb-2">{trackName}</li>
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Playlist</h1>
+      <ul className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {playlistTracks?.map((track, index) => (
+          <li key={index} className="bg-white shadow rounded p-4">
+            <h2 className="text-xl font-semibold mb-2">{track.name}</h2>
+            <p className="text-gray-600 mb-2">{track.artist}</p>
+            <a
+              href={track.spotifyURL}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:underline mb-2"
+            >
+              Spotify Link
+            </a>
+            <img src={track.imageURL} alt={track.name} className="w-full h-auto" />
+          </li>
         ))}
       </ul>
     </div>
   );
-}
+};
 
 export default Playlist;
